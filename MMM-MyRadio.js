393cfa8afe4a51c4838897e47c2b2335e9b167fc
Wolfgang Apolinarski <wolfgang.apolinarski@googlemail.com>
2019-10-06 16:51:32 +0200

    Refactored and added volume and autoplay options

--- MMM-MyRadio.js
+++ MMM-MyRadio.js
@@ -8,6 +8,19 @@
 
 var statix = 0;
 
+// function to get next channel script name
+function getNextChannel(configInstance) {
+	let stationsArray = configInstance.stations;
+	let strsplit = stationsArray[statix].split(" ")
+	let scriptfile = strsplit[1];
+	
+	// increment global variable
+	statix = statix + 1;
+	if (statix === 4) {
+		statix = 0;
+	}
+	return scriptfile;
+};
 Module.register("MMM-MyRadio",{
 
 // Default module config.
@@ -15,8 +28,11 @@ Module.register("MMM-MyRadio",{
 		maxWidth: "100%",
 		updateInterval: 10 * 1000, 
 		stations: [
+			"Bayern1 Bayern1", // Example
 			"R radio", // Separation by space, First part "R" is the .png image filename, the second is .sh script name
-		]
+		],
+		autoplay: false,
+		volume: "100%"
 
 	},
 
@@ -37,13 +53,25 @@ Module.register("MMM-MyRadio",{
           Log.info("Starting module: " + this.name);
   
           // Set locale.
-          var self = this;
+//          var self = this;
           this.scheduleUpdate();
           this.SText = "warte...";
 	  this.TText = "...";
 	  this.VText = "..%";
+	  this.volumeSet(this.config.volume);
+	  if(this.config.autoplay === true) {
+		this.play(getNextChannel(this.config));
+	  }
         },
 
+	play: function(scriptfilename) {
+		this.sendSocketNotification("ChannelSet", scriptfilename);
+	},
+
+	volumeSet: function(volume) {
+		this.sendSocketNotification("VolumeSet", volume);
+	},
+	
 	processSText: function(data) {
          	this.SText = data;
 		this.updateDom();
@@ -81,12 +109,7 @@ Module.register("MMM-MyRadio",{
                 this.updateDom(300);
             }
 	    if (notification === "NEXT_RADIO"){
-		var stationsArray = this.config.stations;
-		var strsplit = stationsArray[statix].split(" ")
-		let scriptfile = strsplit[1];
-		statix = statix + 1;
-		if (statix = 4) { statix = 0; }
-		this.sendSocketNotification(scriptfile, {});
+		this.play(getNextChannel(this.config));
             }
 	    if (notification === "RADIO_STOP"){
                 this.sendSocketNotification("Radiostop", {});
@@ -111,45 +134,45 @@ Module.register("MMM-MyRadio",{
 	// Override dom generator.
 	getDom: function() {
 
-		var self = this;
-		var stationsArray = self.config.stations;
-		var stationListWidth;
+		let self = this;
+		let stationsArray = self.config.stations;
+		let stationListWidth;
 		if (stationsArray.length < 4) {stationListWidth = 292;}
 		else {stationListWidth = ( stationsArray.length * 60 ) + 2;}
 
-		var wrapper = document.createElement("div");
+		let wrapper = document.createElement("div");
 		wrapper.style.maxWidth = this.config.maxWidth;
 
 				
-  	        var stationMenu = document.createElement("div");
+  	        let stationMenu = document.createElement("div");
 		stationMenu.className = "stationMenu";
 		stationMenu.style.maxWidth = stationListWidth;
 
-		var statindex;
+		let statindex;
 		for (statindex = 0; statindex < stationsArray.length; ++statindex) {
-			var strsplit = stationsArray[statindex].split(" ")
+			let strsplit = stationsArray[statindex].split(" ")
 			let scriptfile = strsplit[1];
-			var station = strsplit[1];
-                        var statplace = (statindex * 1) * 60 + 1;
-				station = document.createElement("div");
-				station.innerHTML = '<img src="modules/MMM-MyRadio/images/' + strsplit[0] + '.png" style="cursor:pointer">';
-				station.className = "button";
-				station.style.left = statplace + "px";
-				station.addEventListener("click", () => play(scriptfile));
-				stationMenu.appendChild(station);
+			let station = strsplit[1];
+                        let statplace = (statindex * 1) * 60 + 1;
+			station = document.createElement("div");
+			station.innerHTML = '<img src="modules/MMM-MyRadio/images/' + strsplit[0] + '.png" style="cursor:pointer">';
+			station.className = "button";
+			station.style.left = statplace + "px";
+			station.addEventListener("click", () => self.play(scriptfile));
+			stationMenu.appendChild(station);
 		};
 
 		wrapper.appendChild(stationMenu);
 
-		function play(scriptfile) {
-			self.sendSocketNotification(scriptfile, {});
-		};
+//		function play(scriptfile) {
+//			self.sendSocketNotification(scriptfile, {});
+//		};
 
-		var topMenu = document.createElement("div");
+		let topMenu = document.createElement("div");
 			topMenu.className = "topMenu";
 			topMenu.style.display = "block";
 
-		var stopper = document.createElement("div");
+		let stopper = document.createElement("div");
 			stopper.className = "button";
 			stopper.innerHTML = '<img src="modules/MMM-MyRadio/images/stopButton.png" style="cursor:pointer"></img>';
 			stopper.style.left = "1px";
@@ -159,22 +182,22 @@ Module.register("MMM-MyRadio",{
 			self.sendSocketNotification('Radiostop', {});
 		};
 
-		var volumeUp = document.createElement("div");
+		let volumeUp = document.createElement("div");
 			volumeUp.className = "button";
 			volumeUp.innerHTML = '<img src="modules/MMM-MyRadio/images/volumedownButton.png" style="cursor:pointer"></img>';
 			volumeUp.style.left = "61px";
 			volumeUp.addEventListener("click", () => volumecontrol('VolumeDown'));
-		var volumeDown = document.createElement("div");
+		let volumeDown = document.createElement("div");
 			volumeDown.className = "button";
 			volumeDown.innerHTML = '<img src="modules/MMM-MyRadio/images/volumeupButton.png" style="cursor:pointer"></img>';
 			volumeDown.style.left = "121px";
 			volumeDown.addEventListener("click", () => volumecontrol('VolumeUp'));
-		var volumeMute = document.createElement("div");
+		let volumeMute = document.createElement("div");
 			volumeMute.className = "button";
 			volumeMute.innerHTML = '<img src="modules/MMM-MyRadio/images/muteButton.png" style="cursor:pointer"></img>';
 			volumeMute.style.left = "181px";
 			volumeMute.addEventListener("click", () => volumecontrol('Mute'));
-		var Leer = document.createElement("div");
+		let Leer = document.createElement("div");
 	        	Leer.classList.add("small", "bright", "leer");
 			Leer.innerHTML = "Vol:  <br>" + self.VText;
 			Leer.style.left = "241px";
@@ -191,18 +214,17 @@ Module.register("MMM-MyRadio",{
 		
 		wrapper.appendChild(topMenu);
 
-		var Sender = document.createElement("div");
+		let Sender = document.createElement("div");
 		Sender.classList.add("small", "bright", "sender");
 		Sender.innerHTML = self.SText;
 		wrapper.appendChild(Sender);
 
-		var Titel = document.createElement("div");
+		let Titel = document.createElement("div");
 		Titel.classList.add("xsmall", "bright", "titel");
 		Titel.innerHTML = self.TText;
 		wrapper.appendChild(Titel);
 		
 		return wrapper;
-
 	}
 
 });
